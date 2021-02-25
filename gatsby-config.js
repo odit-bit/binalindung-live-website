fec9919f74a4f3ed30b253bc84e3df6882b28636
Gema Adhi <geadwi@gmail.com>
2021-02-25 11:50:08 +0700

    change config environment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+if (process.env != production) {
+  require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+  })
+}
 
 module.exports = {
   /* Your site config here */
